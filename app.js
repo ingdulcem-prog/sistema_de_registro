d4835e1dbfbd85da253d9fa8cda549ff707b3ee3
ingdulcem-prog <ing.dulcem@gmail.com>
2025-09-23 22:50:58 -0600

    Crea amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Ejecutando el sistema")
+ const inputAmigo =document.getElementById ("amigo");
+  const ListaAmigos =[];
+  const uListaAmigos = document.getElementById ("listaAmigos");
+ 
+  fuction agregarAmigo () {
+ listaAmigos.push (inputAmigo.value);
+
+ uListaAmigos.innerHTML += '<li>${inputAmigo.value} </li>';
+  }
+  fuction sortearAmigo(){
+    
+  }
+  const random =Math.floor(Math.random() * listaAmigos.length);
+  Const amigoSecreto = listaAmigos [random];
+  ulResultado.innerHTML ='<li>El amigo secreto es:${amigoSecreto}</li>';
